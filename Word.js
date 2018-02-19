58368ffc0680a39e1ab8a0a61cefdd9d1d1c0c8a
NI GUO <guoni0909@gmail.com>
2018-02-19 14:05:49 -0800

    update files

--- Word.js
+++ Word.js
@@ -4,29 +4,76 @@ var Word = function(word) {
 	// An array of new Letter objects representing the letters of the underlying word
 	this.newLetter = [];
 	this.word = word;
+	this.wordGet = false;
+	var that = this;
 
 	// call the function on each letter object (the first function defined in Letter.js) 
 	  // that displays the character or an underscore and concatenate those together.
 	this.addLetters = function() {
-		for (var i = 0; i < this.word.length; i++) {
-			var newLetters = new Letters(this.word[i]);
+		for (var i = 0; i < that.word.length; i++) {
+			var newLetters = new Letters(that.word[i]);
 			this.newLetter.push(newLetters);
 		}
 	};
 
+	//If the player does not find a letter
+	this.guesses = function(guessed) {
+		if(this.newLetter.every(function(char){
+			return this.appear === true;
+		})) {
+			this.wordGet = true;
+			return true;
+		}
 
-	// A function that takes a character as an argument 
-	//   // and calls the guess function on each letter object (the second function defined in Letter.js)
-	// this.guesses = function(guessed) {
+	};
+
+	this.checkIfLetterFound = function(guessedLetter) {
+		
+		console.log(this.newletter);
+	   //iterates through each letter to see if it matches the guessed letter
+	   	this.newletter.forEach(function(char){
+	    	if(char.character === guessedLetter){
+		     	char.appear = true;
+		     
+	    	}
+	   	});
+	
+
+	};
+
+	this.wordRender = function() {
+		var showLetter = " ";
+		// for (var = 0; i < this.newLetter.length; i++) {
+		// 	showLetter += this.newLetter.length
+		// }
+		that.newLetter.forEach(function(char){
+			var currentLetters = char.letterRender();
+			showLetter += currentLetters;
+		});
+		return showLetter;
+	};
+}
 
 
-	// }
-
-};
-
+module.exports = Word;
 
 var test = new Word("abc");
 
-console.log(test.addLetters);
 
-module.exports = Word;
+console.log("addLetters: " + test.addLetters());
+console.log("----------------------------------------");
+
+console.log(test.newLetter);
+console.log("----------------------------------------");
+
+console.log("guesses: " + test.guesses());
+console.log("----------------------------------------");
+
+
+console.log("checkIfLetterFound: " + test.checkIfLetterFound("a"));
+console.log("----------------------------------------");
+
+
+console.log("wordRender: " + test.wordRender());
+console.log("----------------------------------------");
+
