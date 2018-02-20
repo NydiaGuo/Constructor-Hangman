419dd21d3f783768a568f011a69cf0a01c8936f0
NI GUO <guoni0909@gmail.com>
2018-02-20 10:01:34 -0800

    update file
    clean up the code

--- Letter.js
+++ Letter.js
@@ -19,37 +19,4 @@ var Letter = function(char) {
 };
 
 
-	// // A function that returns the underlying character if the letter has been guessed, 
-	//  // or a placeholder (like an underscore) if the letter has not been guessed
-	// this.letterInput= function() {
-	// 	//if user puts any letters, returns the input letters
-	// 	if (this.appear) {
-	// 		return this.char;
-	// 	//if they do not put any letters, it returns underscores
-	// 	} 
-	// 	return "_";
-	// };
-		
-	// // A function that takes a character as an argument and checks it against the underlying character,
- // 	// updating the stored boolean value to true if it was guessed correctly
-	// this.checkLetters = function(letter) {
-	// 	//if the input letter in the letter array, show the letters
-	// 	if (letter === this.char) {
-	// 		this.appear = true;
-	// 	} 
-	// };
-
-
-// //users input for testing
-// var a = new Letter('a');
-
-// // // input.letterInput() // returns '_'
-// console.log(a.letterInput());
-
-// // // input.checkLetters("a") // returns true
-// console.log(a.checkLetters('a'));
-
-// // // input.letterInput(); //returns "a"
-// console.log(a.letterInput());
-
 module.exports = Letter;
